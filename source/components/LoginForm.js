e22a28ca69b28def5e2a2d6962b31435ef033481
Mashchenko Oleksandra <dremka@hotmail.com>
2019-03-22 21:00:08 +0200

    login-form init

--- source/components/LoginForm.js
+++ source/components/LoginForm.js
@@ -8,6 +8,50 @@ import Styles from './styles.m.css';
 
 export class LoginForm extends Component {
     render() {
-        return 'Добро пожаловать!';
+        const { touched, isSubmitting, errors, values } = this.props;
+
+        const invalidEmailStyles = cx({
+            [ Styles.invalidInput ]: touched.email && errors.email,
+        });
+
+        const invalidPasswordStyles = cx({
+            [ Styles.invalidInput ]: touched.password && errors.password,
+        });
+
+        const disabledButtonStyle = cx({
+            [ Styles.disabledButton ]: isSubmitting,
+        });
+
+        const submittingMessage = isSubmitting ? 'Вход...' : 'Войти';
+
+        return (
+            <Form>
+                <Field
+                    className = { invalidEmailStyles }
+                    name = 'email'
+                    placeholder = 'Почта'
+                    type = 'email'
+                />
+                <Field
+                    className = { invalidPasswordStyles }
+                    name = 'password'
+                    placeholder = 'Пароль'
+                    type = 'password'
+                />
+                <label>
+                    <Field
+                        checked = { values.remember }
+                        name = 'remember'
+                        type = 'checkbox'
+                    />
+                </label>
+                <span>Запомнить меня</span>
+                <button
+                    className = { disabledButtonStyle }
+                    disabled = { isSubmitting }
+                    type = 'submit'>{submittingMessage}
+                </button>
+            </Form>
+        );
     }
 }
